2df7fb733944dc2cffc3bed431c9594f7539322f
Joao Solano <joaolucassilvasolano@gmail.com>
2025-08-11 08:31:44 -0300

    update notes routes

--- backend/controllers/notes.py
+++ backend/controllers/notes.py
@@ -2,11 +2,16 @@ import asyncpg
 from models.note import Note
 
 async def list_notes(db_conn: asyncpg.Connection):
-    notes = await db_conn.fetch("SELECT * FROM notes")
-    return {"data": notes}
+    return await db_conn.fetch("SELECT * FROM notes")
             
 async def create_note(db_conn: asyncpg.Connection, note: Note):
     query = "INSERT INTO notes (title, content, user_id) VALUES ($1, $2, $3) RETURNING *"
-    new_note = await db_conn.fetchrow(query, note.title, note.content, note.user_id)
-    
-    return {"note": new_note}
+    return await db_conn.fetchrow(query, note.title, note.content, note.user_id)
+
+async def edit_note(db_conn: asyncpg.Connection, note_id: int, note: Note):
+    query = "UPDATE notes SET title = $1, user_id = $2, content = $3 WHERE id = $4 RETURNING *"    
+    return await db_conn.fetchrow(query, note.title, note.user_id, note.content, note_id)
+
+async def delete_note(db_conn: asyncpg.Connection, note_id: int):
+    query = "DELETE FROM notes WHERE id = $1 RETURNING *"    
+    return await db_conn.fetchrow(query, note_id)
--- backend/routers/notes.py
+++ backend/routers/notes.py
@@ -10,15 +10,27 @@ router =  APIRouter()
 @router.get("/")
 async def list_notes(db_conn: asyncpg.Connection = Depends(get_db_conn)):
     try:
-        notes = await notes_controller.list_notes(db_conn)
-        return {"data": notes}
+        return await notes_controller.list_notes(db_conn)
     except Exception as e:
         raise HTTPException(status_code=500, detail=str(e))
             
 @router.post("/")
 async def create_note(note: Note, db_conn: asyncpg.Connection = Depends(get_db_conn)):
     try:
-        new_note = await notes_controller.create_note(db_conn, note)
-        return {"note": new_note}
+        return await notes_controller.create_note(db_conn, note)
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=str(e))
+    
+@router.put("/{note_id}")
+async def edit_note(note_id: int, note: Note, db_conn: asyncpg.Connection = Depends(get_db_conn)):
+    try:
+        return await notes_controller.edit_note(db_conn, note_id, note)
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=str(e))
+    
+@router.delete("/{note_id}")
+async def delete_note(note_id: int, db_conn: asyncpg.Connection = Depends(get_db_conn)):
+    try:
+        return await notes_controller.delete_note(db_conn, note_id)
     except Exception as e:
         raise HTTPException(status_code=500, detail=str(e))
