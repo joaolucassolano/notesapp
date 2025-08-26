bbb9c2e139d7f07ba209e169abf960f56c92a8e1
Joao Solano <joaolucassilvasolano@gmail.com>
2025-08-26 09:30:34 -0300

    Fix: Stop tracking __pycache__ files and update .gitignore

--- backend/controllers/notes.py
+++ backend/controllers/notes.py
@@ -8,9 +8,9 @@ async def get_note(db_conn: asyncpg.Connection, note_id: int):
     query = "SELECT * FROM notes where id = $1"  
     return await db_conn.fetchrow(query, note_id)
             
-async def create_note(db_conn: asyncpg.Connection, note: Note, user_id: str):
+async def create_note(db_conn: asyncpg.Connection, note: dict):
     query = "INSERT INTO notes (title, content, user_id) VALUES ($1, $2, $3) RETURNING *"
-    return await db_conn.fetchrow(query, note.title, note.content, user_id)
+    return await db_conn.fetchrow(query, note["title"], note["content"], note["user_id"])
 
 async def edit_note(db_conn: asyncpg.Connection, note_id: int, note: Note):
     query = "UPDATE notes SET title = $1, content = $2 WHERE id = $3 RETURNING *"    
